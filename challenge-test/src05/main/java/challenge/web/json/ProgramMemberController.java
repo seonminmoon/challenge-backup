90029ce58fc8169c5af5db6a73a3e7becb4d11cf
seonminmoon <se0nmin@naver.com>
2018-07-24 18:44:37 +0900

    .

---
+++ challenge-test/src05/main/java/challenge/web/json/ProgramMemberController.java
@@ -0,0 +1,52 @@
+package challenge.web.json;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import challenge.domain.ProgramMember;
+import challenge.service.ProgramMemberService;
+
+@RestController
+@RequestMapping("/programMember")
+public class ProgramMemberController {
+
+    ProgramMemberService programMemberService;
+
+    public ProgramMemberController(ProgramMemberService programMemberService) {
+        this.programMemberService = programMemberService;
+    }
+        
+    @RequestMapping("list")
+    public Object list() {
+        return programMemberService.list();
+    }
+    
+    @RequestMapping("list/{no}")
+    public Object list(@PathVariable int no) {
+        return programMemberService.list(no);
+    }
+    
+    @RequestMapping("{no}/{userNo}")
+    public List<ProgramMember> view(
+            @PathVariable int no,
+            @PathVariable int userNo) throws Exception {
+        return programMemberService.get(no, userNo);
+    }
+    
+    @RequestMapping("{userNo}")
+    public List<ProgramMember> listWithUserNo(
+            @PathVariable int userNo) throws Exception {
+        return programMemberService.getWithUserNo(userNo);
+    }
+}
+
+//ver 55 - JSON 데이터를 출력하는 페이지 컨트롤러 생성
+
+
+
+
+
+
