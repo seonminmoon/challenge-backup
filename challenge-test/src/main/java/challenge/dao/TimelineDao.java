0f72890da383e26baf64e553317cd48ef5bc8108
seonminmoon <se0nmin@naver.com>
2018-07-25 17:14:19 +0900

    .

---
+++ challenge-test/src/main/java/challenge/dao/TimelineDao.java
@@ -0,0 +1,14 @@
+package challenge.dao;
+
+import java.util.List;
+
+import challenge.domain.Timeline;
+
+public interface TimelineDao {
+    int delete(int no);
+    List<Timeline> selectList();
+    int insert(Timeline timeline);
+    int update(Timeline timeline);
+    Timeline selectOne(int no);
+    int timelineLike(int no);
+}
